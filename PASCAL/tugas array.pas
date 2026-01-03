ba5c2d8dec8b3b9fa6df426a80c4ec2a63ea4b43
mhmmdtegarrr <tegarbisaaja26@gmail.com>
2026-01-03 10:14:57 +0700

    a mao kirim tugas revisi
    a cara nambahin foldernya benr apa engga ya a?

--- PASCAL/tugas array.pas
+++ PASCAL/tugas array.pas
@@ -3,6 +3,10 @@ Uses Crt;
 Var
 	nilai: Array[1..2,1..2] of Integer;
 Begin
+	WriteLn('===========================');
+	WriteLn('**** TUGAS  24/12/2025 ****');
+	WriteLn('===========================');
+
 	nilai[1,1] := 10;
 	nilai[1,2] := 20;
 	nilai[1,1] := 30;
